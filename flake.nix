05fbf8c3ee2fa1dcb503f4e58b99ef951d1aec17
AlphaKeks <alphakeks@dawn.sh>
2023-10-14 07:06:22 +0200

    tooling: fix `.editorconfig` and flake urls

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { nixpkgs, utils, ... }: utils.lib.eachDefaultSystem(system: let
