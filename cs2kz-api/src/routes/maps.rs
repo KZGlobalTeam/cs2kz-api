f1edb374cc0e61a1f0defe7e19f89fe3239fd329
AlphaKeks <alphakeks@dawn.sh>
2023-11-08 09:42:13 +0100

    wip: `/maps` POST endpoint
    probably doesn't work

--- cs2kz-api/src/routes/maps.rs
+++ cs2kz-api/src/routes/maps.rs
@@ -9,9 +9,10 @@ use {
 		Json,
 	},
 	chrono::{DateTime, Utc},
-	cs2kz::{MapIdentifier, PlayerIdentifier, SteamID},
+	cs2kz::{MapIdentifier, Mode, PlayerIdentifier, SteamID, Style, Tier},
 	serde::{Deserialize, Serialize},
 	sqlx::QueryBuilder,
+	std::collections::HashMap,
 	utoipa::{IntoParams, ToSchema},
 };
 
@@ -191,20 +192,52 @@ pub async fn get_map(state: State, Path(ident): Path<MapIdentifier<'_>>) -> Resp
 pub struct NewMap {
 	name: String,
 	workshop_id: u32,
+	courses: Vec<Course>,
 	filters: Vec<Filter>,
-	created_by: SteamID,
+	filesize: u64,
+	owned_by: SteamID,
 	approved_by: SteamID,
 }
 
 #[derive(Debug, Serialize, Deserialize, ToSchema)]
-pub struct Filter {}
+pub struct Course {
+	stage: u8,
+	difficulty: Tier,
+	created_by: SteamID,
+}
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct Filter {
+	stage: u8,
+	mode: Mode,
+	style: Style,
+}
 
 #[derive(Debug, Serialize, Deserialize, ToSchema)]
 pub struct NewMapWithId {
 	id: u16,
+	workshop_id: u32,
+	courses: Vec<CourseWithId>,
+	filters: Vec<FilterWithId>,
+	filesize: u64,
+	owned_by: SteamID,
+	approved_by: SteamID,
+}
 
-	#[serde(flatten)]
-	map: NewMap,
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct CourseWithId {
+	id: u32,
+	stage: u8,
+	difficulty: Tier,
+	created_by: SteamID,
+}
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct FilterWithId {
+	id: u32,
+	stage: u8,
+	mode: Mode,
+	style: Style,
 }
 
 #[tracing::instrument(level = "DEBUG")]
@@ -216,9 +249,141 @@ pub struct NewMapWithId {
 ))]
 pub async fn create_map(
 	state: State,
-	Json(NewMap { name, workshop_id, filters, created_by, approved_by }): Json<NewMap>,
+	Json(NewMap { name, workshop_id, courses, filters, filesize, owned_by, approved_by }): Json<
+		NewMap,
+	>,
 ) -> Result<Created<Json<NewMapWithId>>> {
-	todo!();
+	let course_has_stage = |stage: u8| courses.iter().any(|course| course.stage == stage);
+
+	// Make sure courses have no gaps in their stages
+	let valid_courses = courses
+		.iter()
+		.enumerate()
+		.all(|(x, _)| course_has_stage(x as u8));
+
+	if !valid_courses {
+		todo!();
+	}
+
+	// Make sure each filter actually refers to an existing course
+	let valid_filters = filters
+		.iter()
+		.all(|filter| course_has_stage(filter.stage));
+
+	if !valid_filters {
+		todo!();
+	}
+
+	let mut transaction = state.database().begin().await?;
+
+	sqlx::query! {
+		r#"
+		INSERT INTO
+			Maps (name, workshop_id, filesize, owned_by)
+		VALUES
+			(?, ?, ?, ?)
+		"#,
+		name,
+		workshop_id,
+		filesize,
+		owned_by.as_u32(),
+	}
+	.execute(transaction.as_mut())
+	.await?;
+
+	let map = sqlx::query!("SELECT * FROM Maps WHERE id = (SELECT MAX(id) id FROM Maps)")
+		.fetch_one(transaction.as_mut())
+		.await?;
+
+	let mut create_courses =
+		QueryBuilder::new("INSERT INTO Courses (map_id, stage, difficulty, created_by)");
+
+	create_courses.push_values(&courses, |mut query, course| {
+		query
+			.push_bind(map.id)
+			.push_bind(course.stage)
+			.push_bind(course.difficulty as u8)
+			.push_bind(course.created_by.as_u32());
+	});
+
+	create_courses
+		.build()
+		.execute(transaction.as_mut())
+		.await?;
+
+	let courses = sqlx::query!("SELECT * FROM Courses WHERE map_id = ?", map.id)
+		.fetch_all(transaction.as_mut())
+		.await?
+		.into_iter()
+		.map(|course| (course.stage, course))
+		.collect::<HashMap<_, _>>();
+
+	let min_course_id = courses.values().map(|course| course.id).min();
+	let max_course_id = courses.values().map(|course| course.id).max();
+
+	let mut create_filters =
+		QueryBuilder::new("INSERT INTO Filters (course_id, mode_id, style_id)");
+
+	create_filters.push_values(&filters, |mut query, filter| {
+		let course_id = courses
+			.get(&filter.stage)
+			.map(|course| course.id)
+			.expect("we made sure filters refer to valid courses");
+
+		query
+			.push_bind(course_id)
+			.push_bind(filter.mode as u8)
+			.push_bind(filter.style as u8);
+	});
+
+	create_filters
+		.build()
+		.execute(transaction.as_mut())
+		.await?;
+
+	transaction.commit().await?;
+
+	let filters = sqlx::query!(
+		"SELECT * FROM Filters where course_id < ? AND course_id > ?",
+		min_course_id,
+		max_course_id,
+	)
+	.fetch_all(state.database())
+	.await?;
+
+	let courses = courses
+		.into_values()
+		.map(|course| CourseWithId {
+			id: course.id,
+			stage: course.stage,
+			difficulty: course.difficulty.try_into().unwrap(),
+			created_by: SteamID::from_id32(course.created_by).unwrap(),
+		})
+		.collect::<Vec<_>>();
+
+	let filters = filters
+		.into_iter()
+		.map(|filter| FilterWithId {
+			id: filter.id,
+			stage: courses
+				.iter()
+				.find(|course| course.id == filter.course_id)
+				.map(|course| course.stage)
+				.unwrap(),
+			mode: filter.mode_id.try_into().unwrap(),
+			style: filter.style_id.try_into().unwrap(),
+		})
+		.collect::<Vec<_>>();
+
+	Ok(Created(Json(NewMapWithId {
+		id: map.id,
+		workshop_id,
+		courses,
+		filters,
+		filesize,
+		owned_by,
+		approved_by,
+	})))
 }
 
 #[derive(Debug, Serialize, Deserialize, ToSchema)]
