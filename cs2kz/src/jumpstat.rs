5605f2b1254660316ba10e02941cbbd8309bf553
AlphaKeks <alphakeks@dawn.sh>
2023-11-10 18:41:49 +0100

    fix: update `Jumpstat` enum to match new db schema

--- cs2kz/src/jumpstat.rs
+++ cs2kz/src/jumpstat.rs
@@ -8,17 +8,23 @@ use {
 pub enum Jumpstat {
 	#[cfg_attr(feature = "utoipa", schema(rename = "longjump"))]
 	LongJump = 1,
-	#[cfg_attr(feature = "utoipa", schema(rename = "bhop"))]
-	BunnyHop = 2,
+
+	#[cfg_attr(feature = "utoipa", schema(rename = "single_bhop"))]
+	SingleBhop = 2,
+
 	#[cfg_attr(feature = "utoipa", schema(rename = "multi_bhop"))]
-	MultiBunnyHop = 3,
+	MultiBhop = 3,
+
 	#[cfg_attr(feature = "utoipa", schema(rename = "drop_bhop"))]
-	DropBunnyHop = 4,
-	#[cfg_attr(feature = "utoipa", schema(rename = "weird_jump"))]
+	DropBhop = 4,
+
+	#[cfg_attr(feature = "utoipa", schema(rename = "weirdjump"))]
 	WeirdJump = 5,
-	#[cfg_attr(feature = "utoipa", schema(rename = "ladder_jump"))]
+
+	#[cfg_attr(feature = "utoipa", schema(rename = "ladderjump"))]
 	LadderJump = 6,
-	#[cfg_attr(feature = "utoipa", schema(rename = "ladder_hop"))]
+
+	#[cfg_attr(feature = "utoipa", schema(rename = "ladderhop"))]
 	LadderHop = 7,
 }
 
@@ -26,12 +32,12 @@ impl Jumpstat {
 	pub const fn api(&self) -> &'static str {
 		match self {
 			Jumpstat::LongJump => "longjump",
-			Jumpstat::BunnyHop => "bhop",
-			Jumpstat::MultiBunnyHop => "multi_bhop",
-			Jumpstat::DropBunnyHop => "drop_bhop",
-			Jumpstat::WeirdJump => "weird_jump",
-			Jumpstat::LadderJump => "ladder_jump",
-			Jumpstat::LadderHop => "ladder_hop",
+			Jumpstat::SingleBhop => "single_bhop",
+			Jumpstat::MultiBhop => "multi_bhop",
+			Jumpstat::DropBhop => "drop_bhop",
+			Jumpstat::WeirdJump => "weirdjump",
+			Jumpstat::LadderJump => "ladderjump",
+			Jumpstat::LadderHop => "ladderhop",
 		}
 	}
 }
@@ -50,9 +56,9 @@ macro_rules! try_from {
 			fn try_from(value: $t) -> $crate::Result<Self> {
 				match value {
 					1 => Ok(Self::LongJump),
-					2 => Ok(Self::BunnyHop),
-					3 => Ok(Self::MultiBunnyHop),
-					4 => Ok(Self::DropBunnyHop),
+					2 => Ok(Self::SingleBhop),
+					3 => Ok(Self::MultiBhop),
+					4 => Ok(Self::DropBhop),
 					5 => Ok(Self::WeirdJump),
 					6 => Ok(Self::LadderJump),
 					7 => Ok(Self::LadderHop),
@@ -90,12 +96,12 @@ impl FromStr for Jumpstat {
 	fn from_str(input: &str) -> Result<Self> {
 		match input {
 			"longjump" => Ok(Self::LongJump),
-			"bhop" => Ok(Self::BunnyHop),
-			"multi_bhop" => Ok(Self::MultiBunnyHop),
-			"drop_bhop" => Ok(Self::DropBunnyHop),
-			"weird_jump" => Ok(Self::WeirdJump),
-			"ladder_jump" => Ok(Self::LadderJump),
-			"ladder_hop" => Ok(Self::LadderHop),
+			"bhop" | "single_bhop" => Ok(Self::SingleBhop),
+			"multi_bhop" => Ok(Self::MultiBhop),
+			"drop_bhop" => Ok(Self::DropBhop),
+			"weirdjump" => Ok(Self::WeirdJump),
+			"ladderjump" => Ok(Self::LadderJump),
+			"ladderhop" => Ok(Self::LadderHop),
 			_ => Err(Error::InvalidJumpstat { input: input.to_owned(), reason: None }),
 		}
 	}
