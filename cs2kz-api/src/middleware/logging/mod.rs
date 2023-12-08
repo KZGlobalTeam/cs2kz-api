eff11e1790663d1728e3c60425f69a41216f079c
AlphaKeks <alphakeks@dawn.sh>
2023-12-08 03:35:02 +0100

    refactor: rework logging middleware
    - use `tracing::instrument`
    - log response as well

--- cs2kz-api/src/middleware/logging/mod.rs
+++ cs2kz-api/src/middleware/logging/mod.rs
@@ -1,31 +1,68 @@
-use std::fmt::Write;
 use std::net::SocketAddr;
 
+use axum::body::Body;
 use axum::extract::{ConnectInfo, Request};
 use axum::middleware::Next;
 use axum::response::Response;
+use http_body_util::BodyExt;
 use serde_json::Value as JsonValue;
-use tracing::info;
 
-use crate::{middleware, Result};
+use crate::{Error, Result};
 
 /// Logs basic information about an incoming request.
+#[tracing::instrument(skip_all, fields(
+	method = %request.method(),
+	path = %request.uri(),
+	request_addr = %addr,
+	request_body,
+	response_status,
+	response_body,
+))]
 pub async fn log_request(
 	ConnectInfo(addr): ConnectInfo<SocketAddr>,
 	request: Request,
 	next: Next,
 ) -> Result<Response> {
-	let method = request.method();
-	let uri = request.uri();
-	let mut message = format!("{method} `{uri}` from {addr}");
+	let span = tracing::Span::current();
 
-	let (body, request) = middleware::deserialize_body::<JsonValue>(request).await?;
+	let (parts, body) = request.into_parts();
 
-	if let Some(value) = body {
-		write!(&mut message, " with {value:#?}").expect("this never fails");
+	let body = inspect_body(body, |json| {
+		span.record("request_body", json.to_string());
+	})
+	.await?;
+
+	let request = Request::from_parts(parts, body);
+	let response = next.run(request).await;
+	let (parts, body) = response.into_parts();
+
+	span.record("response_status", parts.status.to_string());
+
+	let body = inspect_body(body, |json| {
+		span.record("response_body", json.to_string());
+	})
+	.await?;
+
+	let response = Response::from_parts(parts, body);
+
+	Ok(response)
+}
+
+/// Consumes an HTTP body and attempts to deserialize it as JSON.
+/// If that succeeds, it calls the supplied `then` function with the JSON data.
+async fn inspect_body<F>(body: Body, mut then: F) -> Result<Body>
+where
+	F: FnMut(JsonValue),
+{
+	let bytes = body
+		.collect()
+		.await
+		.map_err(|_| Error::InvalidRequestBody)?
+		.to_bytes();
+
+	if let Ok(data) = serde_json::from_slice(&bytes) {
+		then(data);
 	}
 
-	info!("{message}");
-
-	Ok(next.run(request).await)
+	Ok(Body::from(bytes))
 }
