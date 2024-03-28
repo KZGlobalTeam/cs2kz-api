00a7f6c42f5685d1bb8632fc646465b33e508395
AlphaKeks <alphakeks@dawn.sh>
2024-03-28 18:08:00 +0100

    feat: more default data in migrations

--- database/migrations/99999999999999_data.up.sql
+++ database/migrations/99999999999999_data.up.sql
@@ -105,7 +105,7 @@ VALUES
     76561198282622073,
     "AlphaKeks",
     "127.0.0.1",
-    (0b10000000000000000000000000000000)
+    (0b10000000000000010000000100000001)
   );
 
 INSERT INTO
@@ -115,8 +115,143 @@ VALUES
     76561198118681904,
     "zer0.k",
     "127.0.0.1",
-    (0b10000000000000000000000000000000)
+    (0b10000000000000010000000100000001)
   );
+
+INSERT INTO
+  `Players` (`id`, `name`, `ip_address`, `role_flags`)
+VALUES
+  (
+    76561198165203332,
+    "GameChaos",
+    "127.0.0.1",
+    (0b10000000000000010000000100000001)
+  );
+
+INSERT INTO
+  `Players` (`id`, `name`, `ip_address`, `role_flags`)
+VALUES
+  (
+    76561198003275951,
+    "Sikari",
+    "127.0.0.1",
+    (0b10000000000000010000000100000001)
+  );
+
+INSERT INTO
+  `Players` (`id`, `name`, `ip_address`, `role_flags`)
+VALUES
+  (
+    76561197989817982,
+    "DanZay",
+    "127.0.0.1",
+    (0b10000000000000010000000100000001)
+  );
+
+INSERT INTO
+  `Players` (`id`, `name`, `ip_address`, `role_flags`)
+VALUES
+  (
+    76561199067702427,
+    "Reeed",
+    "127.0.0.1",
+    (0b10000000000000010000000100000001)
+  );
+
+INSERT INTO
+  `Players` (`id`, `name`, `ip_address`, `role_flags`)
+VALUES
+  (
+    76561198201492663,
+    "makis",
+    "127.0.0.1",
+    (0b10000000000000010000000100000001)
+  );
+
+INSERT INTO
+  `Players` (`id`, `name`, `ip_address`)
+VALUES
+  (
+    76561198260657129,
+    "ReDMooN",
+    "127.0.0.1"
+  );
+
+INSERT INTO
+  `Maps` (
+    `name`,
+    `description`,
+    `global_status`,
+    `workshop_id`,
+    `checksum`
+  )
+VALUES
+  (
+    "kz_grotto",
+    "launders approved",
+    1,
+    3121168339,
+    3429798845
+  );
+
+INSERT INTO
+  `Mappers` (`map_id`, `player_id`)
+VALUES
+  (1, 76561198260657129);
+
+INSERT INTO
+  `Courses` (`map_id`)
+VALUES
+  (1);
+
+INSERT INTO
+  `CourseMappers` (`course_id`, `player_id`)
+VALUES
+  (1, 76561198260657129);
+
+INSERT INTO
+  `CourseFilters` (
+    `course_id`,
+    `mode_id`,
+    `teleports`,
+    `tier`,
+    `ranked_status`
+  )
+VALUES
+  (1, 1, true, 3, 1);
+
+INSERT INTO
+  `CourseFilters` (
+    `course_id`,
+    `mode_id`,
+    `teleports`,
+    `tier`,
+    `ranked_status`
+  )
+VALUES
+  (1, 1, false, 4, 1);
+
+INSERT INTO
+  `CourseFilters` (
+    `course_id`,
+    `mode_id`,
+    `teleports`,
+    `tier`,
+    `ranked_status`
+  )
+VALUES
+  (1, 2, true, 2, 1);
+
+INSERT INTO
+  `CourseFilters` (
+    `course_id`,
+    `mode_id`,
+    `teleports`,
+    `tier`,
+    `ranked_status`
+  )
+VALUES
+  (1, 2, false, 3, 1);
 
 INSERT INTO
   `Servers` (
