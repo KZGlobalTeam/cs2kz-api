5e139c9e56ea063c1df668532b6745ee8d2dcfe7
AlphaKeks <alphakeks@dawn.sh>
2024-03-29 13:47:07 +0100

    refactor: remove bunyan crap

--- src/logging/files.rs
+++ src/logging/files.rs
@@ -3,8 +3,8 @@ use std::{env, fs, io};
 
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_appender::rolling::Rotation;
-use tracing_bunyan_formatter::BunyanFormattingLayer;
 use tracing_subscriber::filter::FilterFn;
+use tracing_subscriber::fmt::format::FmtSpan;
 use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::Layer;
 
@@ -29,11 +29,15 @@ where
 		.map(tracing_appender::non_blocking)
 		.expect("failed to initialize logger");
 
-	let layer = BunyanFormattingLayer::new(String::from("cs2kz-api"), writer).with_filter(
-		FilterFn::new(|metadata| {
+	let layer = tracing_subscriber::fmt::layer()
+		.with_target(true)
+		.with_writer(writer)
+		.with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
+		.compact()
+		.with_ansi(false)
+		.with_filter(FilterFn::new(|metadata| {
 			metadata.target().starts_with("audit_log") || metadata.target().starts_with("cs2kz_api")
-		}),
-	);
+		}));
 
 	Ok((layer, guard, log_dir))
 }
