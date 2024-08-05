b12d9d6d445de5e96f80e61e5bad87876be4a224
AlphaKeks <alphakeks@dawn.sh>
2024-08-05 03:19:23 +0200

    testing: add more utilities

--- src/testing.rs
+++ src/testing.rs
@@ -1,5 +1,24 @@
 //! This module contains helpers for unit/integration tests.
 
+use std::sync::Arc;
+
+use sqlx::{MySql, Pool};
+use url::Url;
+
+use crate::services::{AuthService, SteamService};
+
+pub fn auth_svc(database: Pool<MySql>) -> AuthService
+{
+	let http_client = reqwest::Client::new();
+	let api_url = Arc::new(Url::parse("http://127.0.0.1").unwrap());
+	let steam_api_key = String::new();
+	let steam_svc = SteamService::new(api_url, steam_api_key, None, None, http_client.clone());
+	let jwt_secret = String::from("Zm9vYmFyYmF6");
+	let cookie_domain = String::from("localhost");
+
+	AuthService::new(database, http_client, steam_svc, jwt_secret, cookie_domain).unwrap()
+}
+
 /// Global constructor that will run before tests.
 #[ctor::ctor]
 fn ctor()
@@ -38,10 +57,22 @@ macro_rules! assert {
 	};
 }
 
+macro_rules! assert_eq {
+	($lhs:expr, $rhs:expr $(, $($msg:tt)*)?) => {
+		if &$lhs != &$rhs {
+			let lhs = stringify!($lhs);
+			let rhs = stringify!($rhs);
+			::color_eyre::eyre::bail!(
+				"assertion `{lhs} == {rhs}` failed\n  lhs: {lhs}\n  rhs:{rhs}"
+			);
+		}
+	};
+}
+
 macro_rules! assert_matches {
 	($expr:expr, $pat:pat $(if $cond:expr)? $(, $($msg:tt)*)?) => {
 		::color_eyre::eyre::ensure!(matches!($expr, $pat $(if $cond)? $(, $($msg)*)?))
 	};
 }
 
-pub(crate) use {assert, assert_matches};
+pub(crate) use {assert, assert_eq, assert_matches};
