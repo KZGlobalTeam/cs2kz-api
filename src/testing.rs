f39a2f1c8d0ac1d1d0ae545905e1299cee1e2ef1
AlphaKeks <alphakeks@dawn.sh>
2024-08-05 02:09:30 +0200

    testing: basic testing helpers

--- src/testing.rs
+++ src/testing.rs
@@ -4,8 +4,11 @@
 #[ctor::ctor]
 fn ctor()
 {
+	use std::env;
+
 	use tracing_subscriber::fmt::format::FmtSpan;
 	use tracing_subscriber::EnvFilter;
+	use url::Url;
 
 	color_eyre::install().expect("failed to install color-eyre");
 	tracing_subscriber::fmt()
@@ -21,4 +24,24 @@ fn ctor()
 		.with_thread_names(true)
 		.with_env_filter(EnvFilter::from_default_env())
 		.init();
+
+	if let Ok(database_url) = env::var("DATABASE_URL") {
+		let mut database_url = database_url.parse::<Url>().unwrap();
+		database_url.set_username("root").unwrap();
+		env::set_var("DATABASE_URL", database_url.as_str());
+	}
 }
+
+macro_rules! assert {
+	($expr:expr $(, $($msg:tt)*)?) => {
+		::color_eyre::eyre::ensure!($expr $(, $($msg)*)?)
+	};
+}
+
+macro_rules! assert_matches {
+	($expr:expr, $pat:pat $(if $cond:expr)? $(, $($msg:tt)*)?) => {
+		::color_eyre::eyre::ensure!(matches!($expr, $pat $(if $cond)? $(, $($msg)*)?))
+	};
+}
+
+pub(crate) use {assert, assert_matches};
